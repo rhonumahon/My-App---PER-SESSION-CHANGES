6bf763c87ce67cf254b05f4da48e0628cb09694c
camitsharlynne <53287751+camitsharlynne@users.noreply.github.com>
2021-10-21 12:31:56 +0800

    Add files in root

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/myApp'),
+      dir: require('path').join(__dirname, './coverage/upskilling-assessment-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
